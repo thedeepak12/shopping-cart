1c73420bb79a443cc9f78241bbd967ec8ea56e2d
Deepak <206972401+thedeepak12@users.noreply.github.com>
2025-08-18 10:46:22 +0530

    Setup Vitest

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.js',
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}', 'tests/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+  },
 })
