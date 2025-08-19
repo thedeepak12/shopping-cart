98dd600229955167c4d0a812e5d929c35bdef160
Deepak <206972401+thedeepak12@users.noreply.github.com>
2025-08-19 16:15:18 +0530

    Add api.js

---
+++ src/utils/api.js
@@ -0,0 +1,10 @@
+export const fetchProducts = async () => {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products');
+    if (!response.ok) throw new Error('Failed to fetch products');
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return [];
+  }
+};
